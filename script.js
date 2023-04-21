0733ee239ee9bd4dccc7678da53a0e9d9e48937f
Alona Didyk <alionadiduk31@gmail.com>
2023-04-21 17:51:35 +0300

    final commit

--- script.js
+++ script.js
@@ -36,27 +36,93 @@ const shoppingList = [
     quantity: 2,
     bought: false,
     price: 30,
-    sum: 0
-}
+    sum: 0,
+  },
 ];
 console.log(shoppingList);
 
-const sortedList = shoppingList.sort((a, b) => a.bought - b.bought)
+const sortedList = shoppingList.sort((a, b) => a.bought - b.bought);
 
-sortedList.forEach(item => {
-    const purchased = item.bought ? `куплено` : `не куплено`;
-    alert(`${item.name}: Кількість: ${item.quantity} шт. Ціна: ${item.price} грн. Статус: ${purchased}`)
-})
+sortedList.forEach((item) => {
+  const purchased = item.bought ? `куплено` : `не куплено`;
+  alert(
+    `${item.name}: Кількість: ${item.quantity} шт. Ціна: ${item.price} грн. Статус: ${purchased}`
+  );
+});
 
 const productItem = prompt(`Вкажіть, що ви купили`);
 const boughtItem = shoppingList.find((item) => item.name === productItem);
 
 if (boughtItem) {
-    boughtItem.bought = true;
-    const purchased = boughtItem.bought ? `куплено` : `не куплено`;
-    alert(`${productItem} ${purchased}!`);
+  boughtItem.bought = true;
+  const purchased = boughtItem.bought ? `куплено` : `не куплено`;
+  alert(`${productItem} ${purchased}!`);
 } else {
-    alert(`Продукт "${productItem}" не знайдено в списку покупок`);
+  alert(`Продукт "${productItem}" не знайдено в списку покупок`);
 }
 
 console.log(shoppingList);
+
+// НОРМА
+
+const itemDelete = prompt("Вкажіть назву продукту, який Ви хочете видалити");
+const updatedShoppingList = shoppingList.filter(
+  (item) => item.name !== itemDelete
+);
+
+console.log(updatedShoppingList);
+
+const newItemName = prompt("Введіть назву продукту:");
+const newItemQuantity = +prompt("Введіть кількість:");
+const newItemPrice = +prompt("Введіть ціну:");
+const newItemSum = newItemQuantity * newItemPrice;
+
+let existingItem = updatedShoppingList.find(
+  (item) => item.name === newItemName
+);
+
+if (existingItem) {
+  existingItem.quantity += newItemQuantity;
+} else {
+  let newItem = {
+    name: newItemName,
+    quantity: newItemQuantity,
+    bought: false,
+    price: newItemPrice,
+    sum: newItemSum,
+  };
+  updatedShoppingList.push(newItem);
+}
+
+console.log(updatedShoppingList);
+
+// МАКСИМУМ
+
+let totalSum = 0;
+
+for (let index = 0; index < shoppingList.length; index++) {
+  totalSum += shoppingList[index].quantity * shoppingList[index].price;
+}
+
+alert(`Повна сума: ${totalSum} грн.`);
+
+let boughtSum = 0;
+let notBoughtSum = 0;
+
+for (let index = 0; index < shoppingList.length; index++) {
+  if (shoppingList[index].bought) {
+    boughtSum += shoppingList[index].quantity * shoppingList[index].price;
+  } else {
+    notBoughtSum += shoppingList[index].quantity * shoppingList[index].price;
+  }
+}
+
+alert(`Сума придбаних товарів: ${boughtSum} грн, непридбаних: ${notBoughtSum} грн.`);
+
+shoppingList.forEach (item => {
+    item.sum = item.quantity * item.price;
+})
+
+const sortedListByPrice = shoppingList.sort((a, b) => a.sum - b.sum);
+
+console.log(sortedListByPrice);
