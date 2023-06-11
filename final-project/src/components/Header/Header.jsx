1fe880de04b4852ee356a9ca5a61f071ce048200
Alona Didyk <alionadiduk31@gmail.com>
2023-06-11 18:12:04 +0300

    new commit

--- final-project/src/components/Header/Header.jsx
+++ final-project/src/components/Header/Header.jsx
@@ -1,81 +1,114 @@
-import { Outlet, Link, useNavigate, Navigate } from "react-router-dom";
-
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./Header.scss";
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 export const Header = ({ hideEntryPage }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
+
+  const headerRef = useRef(null);
+  const burgerButtonRef = useRef(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   const navigateHandler = (event, to) => {
     event.preventDefault();
     navigate(to);
   };
 
-  const {currentUser} = useContext(AuthContext);
-  const AuthRoute = ({children}) => {
-    return currentUser ? children : <Navigate to="/login"/>
-  }
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <>
-      <header className="header">
+      <header ref={headerRef} className={`header ${menuOpen ? "open" : ""}`}>
         <div className="header__wrapper">
-            <div className="header__logo">
-            <img className="header__logo--img" src={logo} alt="logo" />
-            </div>
-
-          <nav className="header__nav">
-
-            <ul className="header__nav--ul">
-              {(
-                <li className="header__nav--li">
-                  <Link onClick={(event) => navigateHandler(event, "/")}>
-                    HomePage
-                  </Link>
-                </li>
-              )}
-              {/* <li className="header__nav--li">
-                <Link onClick={(event) => navigateHandler(event, "/homepage")}>
+          <div className="header__subwrapper">
+            <button
+              ref={burgerButtonRef}
+              className="header__burger-button"
+              id="burger-button"
+              onClick={toggleMenu}
+            >
+              <span className="header__burger-button-line"></span>
+              <span className="header__burger-button-line"></span>
+              <span className="header__burger-button-line"></span>
+            </button>
+            <img className="header__logo" src={logo} alt="logo" />
+          </div>
+          <div className="header__container">
+            <ul className="header__nav">
+              <li>
+                <Link
+                to="/"
+                  className="header__nav--item"
+                  onClick={() => setMenuOpen(false)}
+                >
                   HomePage
                 </Link>
-              </li> */}
-
-              <li className="header__nav--li">
-                <Link onClick={(event) => navigateHandler(event, "/movie")}>
+              </li>
+              <li>
+                <Link
+                to="/movie"
+                  className="header__nav--item"
+                  onClick={() => setMenuOpen(false)}
+                >
                   Movie
                 </Link>
               </li>
-              <li className="header__nav--li">
-                <Link onClick={(event) => navigateHandler(event, "/comic")}>
+              <li>
+                <Link
+                to="/comic"
+                  className="header__nav--item"
+                  onClick={() => setMenuOpen(false)}
+                >
                   Comic
                 </Link>
               </li>
-              <li className="header__nav--li">
-                <Link onClick={(event) => navigateHandler(event, "/login")}>
-                  Login
+              <li>
+                <Link
+                to="/chat"
+                  className="header__nav--item"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Community
                 </Link>
               </li>
-              <li className="header__nav--li">
-                <Link onClick={(event) => navigateHandler(event, "/regist")}>
-                  Regist
-                </Link>
-              </li>
-              <li className="header__nav--li">
-                <Link onClick={(event) => navigateHandler(event, "/chat")}>
-                  Chat
-                </Link>
-              </li>
-              <li className="header__nav--li">
- 
-                <Link onClick={(event) => navigateHandler(event, "/user")}>
-                  {currentUser?.displayName}
-                </Link>
-       
-         
-              </li>
+          
             </ul>
-          </nav>
+            <div className="header__buttons">
+                <li>
+                  <Link
+                  to="/login"
+                    className="button header__nav--item"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                  to="/regist"
+                    className="button "
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Regist
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                  to="/user"
+                    className="account"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    {<p> Account:{currentUser?.displayName}</p>}
+                  </Link>
+                </li>
+              </div>
+          </div>
         </div>
       </header>
 
